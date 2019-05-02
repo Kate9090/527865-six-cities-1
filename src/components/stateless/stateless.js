3af614fdf6b59aadf17fff5c037f54303e4b0ee8
eremina-ekaterina <eremina@uchi.ru>
2019-05-02 15:43:10 +0300

    select

--- src/components/stateless/stateless.js
+++ src/components/stateless/stateless.js
@@ -62,12 +62,12 @@ const Stateless = () => {
                 <li className="places__option" tabIndex="0">Top rated first</li>
               </ul>
 
-              {/* <select className="places__sorting-type" id="places-sorting">
-                <option className="places__option" value={`popular`} selected={``}>Popular</option>
+              <select className="places__sorting-type" id="places-sorting" defaultValue={`popular`}>
+                <option className="places__option" value={`popular`}>Popular</option>
                 <option className="places__option" value={`to-high`}>Price: low to high</option>
                 <option className="places__option" value={`to-low`}>Price: high to low</option>
                 <option className="places__option" value={`top-rated`}>Top rated first</option>
-              </select> */}
+              </select>
 
             </form>
             <div className="cities__places-list places__list tabs__content">
