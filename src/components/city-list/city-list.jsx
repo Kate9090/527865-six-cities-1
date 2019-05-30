55e4ed76fda49624fba0f55e83ac9fa648ce3ce1
eremina-ekaterina <eremina@uchi.ru>
2019-05-30 13:41:34 +0300

    prove component

--- src/components/city-list/city-list.jsx
+++ src/components/city-list/city-list.jsx
@@ -1,36 +1,28 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import {connect} from 'react-redux';
 
-class CitiesTopMenu extends PureComponent {
-  constructor(props) {
-    super(props);
-    this._onUserChoose = this._onUserChoose.bind(this);
-  }
+const CitiesTopMenu = (props) => {
+  const {cities} = props;
 
-  _onUserChoose(city, num, evt) {
-    // const {onUserChoose} = this.props;
+  const _onUserChoose = (city, num, evt) => {
     evt.preventDefault();
-    this.props.onUserAnswer(city, num);
-  }
+    props.onUserAnswer(city, num);
+  };
 
-  render() {
-    const {cities} = this.props;
-
-    return <ul className="locations__list tabs__list">
-      {cities.map((it, i) => (
-        // <CityFromTopMenu key={`city-${i}`}/>
-        <li className="locations__item" key={`city-${i}`}>
-          <a onClick={(e) => this._onUserChoose(it.city, i, e)} className="locations__item-link tabs__item" href="#">
-            <span>{it.city}</span>
-          </a>
-        </li>
-      )
-      )}
-    </ul>;
-  }
-}
+  return <ul className="locations__list tabs__list">
+    {cities.map((it, i) => (
+      // <CityFromTopMenu key={`city-${i}`}/>
+      <li className="locations__item" key={`city-${i}`}>
+        <a onClick={(e) => _onUserChoose(it.city, i, e)} className="locations__item-link tabs__item" href="#">
+          <span>{it.city}</span>
+        </a>
+      </li>
+    )
+    )}
+  </ul>;
+};
 
 CitiesTopMenu.propTypes = {
   cities: PropTypes.arrayOf(PropTypes.shape({
